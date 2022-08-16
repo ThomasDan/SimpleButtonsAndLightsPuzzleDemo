7fa15e548af01ac8ca7e237834cde0fbae11409e
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2022-08-16 12:53:21 +0200

    Comments, compactedness, scalability
    Way more comments than originally.
    Made the code smarter and more compact.
    Much better scalability and ease of modification, should one wish to add more lights, buttons, or programs for the buttons.

--- SimpleBtnsAndLightsPuzzle/SimpleBtnsAndLightsPuzzle.ino
+++ SimpleBtnsAndLightsPuzzle/SimpleBtnsAndLightsPuzzle.ino
@@ -1,8 +1,8 @@
 /*
-  A very simple lights-and-buttons puzzle, as a demonstration for some young teenagers of what we might make on my current education.
-  All lights are turned off to begin with. The goal is to turn them all on. It can be done with just 2 button presses.
+  A very simple lights-and-buttons puzzle, as a demonstration, for some young teenagers, of what we might make on my current education.
+  All lights are turned off to begin with. The goal is to turn them all on. As it is a Very simple puzzle of this sort, it can be solved with just 2 button presses.
 
-  Every button affects the lights in pre-determined way.
+  Every button affects the lights in pre-determined way (programs).
 
   Coded for 3 LED lights and 3 buttons. Each button having 2 'programs'.
 
@@ -10,41 +10,49 @@
 
 */
 
+const int numberOfLights = 3;
+const int numberOfButtons = 3;
+const int numberOfPrograms = 2;
+
 class Light{
   public:
-    int pinNumber;
-    bool state;
+    int pinNumber; // The pinNumber of the input for the LED light
+    bool state; // Keeps track of whether the LED is supposed to be ON (True) or OFF (False)
+
     // This the Constructor for the Light class
     Light(int _pinNumber){
       pinNumber = _pinNumber;
-      state = false;
-      pinMode(pinNumber, OUTPUT);
-      digitalWrite(pinNumber, LOW);
+      state = false; // Light should be off by default
+      pinMode(pinNumber, OUTPUT); // Pin initialized
+      digitalWrite(pinNumber, LOW); // Pin set to Off
     }
 
+    // This is used for the buttons' programs. Each Program has an Action for a Light to perform.
     PerformAction(int action){
-      // If turn off, and not already turned off
-      if(action == 0 && state){
-        state = false;
-      } // -||-, but on
-      else if(action == 1 && !state){
-        state = true;
-      } // Flip the Light's state
-      else if(action == 2){
-        state = !state;
+      switch(action){
+        case 0: // Turns the light OFF
+          state = false;
+          break;
+        case 1: // Turns the light ON
+          state = true;
+          break;
+        default: // Flips the light's state from ON to OFF, or OFF to ON. 2 should be the only case, but default is never a bad idea!
+          state = !state;
+          break;
       }
       digitalWrite(pinNumber, state ? HIGH : LOW);
     }
 };
 
+// Perhaps a somewhat misleading name, this is used by the Button class to determien what the buttons do when pressed.
 class Program{
   public: 
-    int lightIndex;
+    int lightIndex; // The index of the Light in the lights array.
     // Action (0: SET TO OFF, 1: SET TO ON, 2: FLIP)
-    int action;
+    int action; // What action that the Light must perform.
     
     Program(){
-
+      // This empty constructor is Necessary, as the buttons must be initialized with empty programs.
     }
     Program(int _light, int _action){
       lightIndex = _light;
@@ -55,10 +63,10 @@ class Program{
 class Button{
   public: 
     int pinNumber;
-    Program programs[2] = { Program(), Program() }; // I wanted to make this array of dynamic sizing, but alas, such is bad practice with arduino.
+    Program programs[numberOfPrograms] = { Program(), Program() }; // I wanted to make this array of dynamic sizing, but alas, such is bad practice with arduino.
     
     Button(){
-
+      // This empty constructor is necessary, as the array of buttons requires that it is initialized with empty buttons.
     }
     Button(int _pinNumber, Program prog1, Program prog2){
       pinNumber = _pinNumber;
@@ -69,110 +77,124 @@ class Button{
     
 };
 
-const Light lights[3] = {
+const Light lights[numberOfLights] = {
   Light(7), Light(6), Light(5)
 };
 
 // Initialization with empty Button()
-const Button buttons[3] = { Button(), Button(), Button() };
+const Button buttons[numberOfButtons] = { Button(), Button(), Button() };
 
 void setup() {
   Serial.begin(9600);
 
-  // Button 1 programming: (L1 ON, Flip L2)
-  Program b1L1 = Program(0, 1);
-  Program b1L2 = Program(1, 2);
-  buttons[0] = Button(4, b1L1, b1L2);
+  // Button 1 programming: (L1 ON, L2 FLIP)
+  Program b1L1 = Program(0, 1); // Light 1 (on index 0 in the lights array), turn ON (program.action = 1)
+  Program b1L2 = Program(1, 2); // Light 2 (on index 1 in the lights array), Flip state (program.action = 2)
+  buttons[0] = Button(4, b1L1, b1L2); // Button(pinNumber, Program, Program)
 
   // Button 2 programming: (Flip L1 & L3)
-  Program b2L1 = Program(0, 2);
-  Program b2L3 = Program(2, 2);
+  Program b2L1 = Program(0, 2); // Light 1 (index 0), Flip state (action 2)
+  Program b2L3 = Program(2, 2); // Light 3 (index 2), Flip state (action 2)
   buttons[1] = Button(3, b2L1, b2L3);
 
   // Button 3 proramming: (L2 OFF, Flip L3)
-  Program b3L2 = Program(1, 0);
-  Program b3L3 = Program(2, 2);
+  Program b3L2 = Program(1, 0); // Light 2 (index 1), turn off (action 0)
+  Program b3L3 = Program(2, 2); // Light 3 (index 2), Flip state (action 2)
   buttons[2] = Button(2, b3L2, b3L3);
 }
 
 void loop() {
   bool gameOver = false;
   
-  
   while(!gameOver){
-    // It is important to update the bX variables like so
-    int b1 = digitalRead(buttons[0].pinNumber);
-    int b2 = digitalRead(buttons[1].pinNumber);
-    int b3 = digitalRead(buttons[2].pinNumber);
-    if(b1 == 1){
-      gameOver = buttonClicked(buttons[0]);
+    // Here we check if any of the buttons are being pressed
+    for(int i = 0; i < numberOfButtons; i++){
+      if(digitalRead(buttons[i].pinNumber) == 1){
+        runButtonPrograms(buttons[i]);
+        gameOver = checkVictory();
+        delay(350);
+        break;
+      }
     }
-    else if(b2 == 1){
-      gameOver = buttonClicked(buttons[1]);
-    }
-    else if(b3 == 1){
-      gameOver = buttonClicked(buttons[2]);
-    }
-
     delay(125);
   }
-
-  delay(1500);
   // Victory Lightshow!
   victoryShow();
-  delay(1500);
-  lightsOut();}
-
-bool buttonClicked(Button btn){
-  Serial.println((String)"Button pin: " + btn.pinNumber);
-  runButtonPrograms(btn);
-  delay(500);
-  return checkVictory();
+  // Set all the lights to off, just prior to restarting the game
+  lightsOut();
 }
 
+// Applies all of a button's programs to the lights
 void runButtonPrograms(Button btn){
-  for(int i = 0; i < 2; i++){
+  for(int i = 0; i < numberOfPrograms; i++){
     Program prog = btn.programs[i];
-    // We call on the Light of this Program's lightIndex, to perform this Program's action
-    lights[prog.lightIndex].PerformAction(prog.action);
+    // NULL check, to ensure this Program is actually used for something, and is not merely leftover from the empty initialization.
+    if(prog.action != NULL && prog.lightIndex != NULL){
+      // We call on the Light of this Program's lightIndex, to perform this Program's action
+      lights[prog.lightIndex].PerformAction(prog.action);
+    }
   }
 }
 
+// Returns true if all lights are on
 bool checkVictory(){
-  return lights[0].state && lights[1].state && lights[2].state;
+  for(int i = 0; i < numberOfLights; i++){
+    if(!lights[i].state){
+      // If a single Light is state false (Off), the puzzle has not been solved, and there is no reason to run through the rest of the lights.
+      return false;
+    }
+  }
+  return true;
 }
 
-void lightsOut(){or(int i = 0; i < 3; i++){
+// Turns off all the lights
+void lightsOut(){
+  for(int i = 0; i < numberOfLights; i++){
     lights[i].PerformAction(0); // Turn the Light off
   }
 }
 
+// A little light show to celebrate user having solved the puzzle
 void victoryShow(){
+  delay(1125);
   lightsOut();
-  delay(500);
-  for(int i = 0; i < 3; i++){
+  delay(300);
+  // From left-to-right, turning the lights on.
+  for(int i = 0; i < numberOfLights; i++){
     lights[i].PerformAction(1);
     delay(250);
   }
   delay(100);
   lightsOut();
-  for(int i = 2; i > -1; i--){
+  // From right-to-left, turning the lights on
+  for(int i = numberOfLights-1; i > -1; i--){
     lights[i].PerformAction(1);
     delay(250);
   }
   delay(100);
+  // Rapidly simultaneously turning even and uneven lights on and off, 10 times over about 2 seconds.
   for(int i = 0; i < 10; i++){
     lightsOut();
-    lights[0].PerformAction(1);
-    lights[2].PerformAction(1);
+    // Lights on even index are turned on
+    for(int j = 0; j < numberOfLights; j++){
+      if(j % 2 == 0){
+        lights[j].PerformAction(1);
+      }
+    }
     delay(100);
     lightsOut();
-    lights[1].PerformAction(1);
+    // Lights on uneven indexes are turned off
+    for(int k = 0; k < numberOfLights; k++){
+      if(k % 2 != 0){
+        lights[k].PerformAction(1);
+      }
+    }
     delay(100);
   }
   lightsOut();
-  lights[0].PerformAction(1);
-  lights[1].PerformAction(1);
-  lights[2].PerformAction(1);
-  delay(350);
+  // All lights are turned on one last time, to better indicate that the puzzle has been reset
+  for(int i = 0; i < numberOfLights; i++){
+    lights[i].PerformAction(1);
+  }
+  delay(1000);
 }
